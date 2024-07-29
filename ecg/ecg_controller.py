26169702bee9a8d03b4582d866cc1e6c6ec8099f
Michael L <contact@michaelmlowe.com>
2024-07-28 18:45:40 -0700

    Improved session management, organizing uploaded files by session user and deleting after access.

--- ecg/ecg_controller.py
+++ ecg/ecg_controller.py
@@ -9,8 +9,20 @@ bp = Blueprint('ecg', __name__, url_prefix='/ecg')
 
 @bp.route('/download/<filename>')
 def download_file(filename):
-    samples = parse_ecg.read_24bit_samples(filename)
-    return samples
+    username = session.get('username', None)
+    username = secure_filename(username)
+    if username is not None and os.path.isdir(f'{current_app.config['UPLOAD_FOLDER']}/{username}/'):
+        samples = parse_ecg.read_24bit_samples(f'{current_app.config['UPLOAD_FOLDER']}/{username}/{filename}')
+
+        # Remove the file after reading it
+        os.remove(f'{current_app.config['UPLOAD_FOLDER']}/{username}/{filename}')
+
+        # If the directory associated with this user contains no files, delete the directory
+        if not os.listdir(f'{current_app.config['UPLOAD_FOLDER']}/{username}/'):
+            os.rmdir(f'{current_app.config['UPLOAD_FOLDER']}/{username}/')
+
+        return samples
+    return 'File not found!'
 
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -32,17 +44,23 @@ def compress_file():
         # Validate file extension and process the file
         if file and validate_file_extensions.validate(file.filename):
             original_size = read_raw_file_size.get_file_size(file)
-            samples = parse_ecg.read_samples(file)
 
             # Compress the ECG data here
 
-            # Prepend the username to the filename
-            filename = secure_filename(username+'_'+file.filename)
-            file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+            # We need to sanitize the filename and username before using them to modify the filesystem
+            filename = secure_filename(file.filename)
+            username = secure_filename(username)
+
+            # We want to check if there exists an upload subdirectory for this session's user
+            # If no path exists, we'll create it
+            os.path.isdir(f'{current_app.config['UPLOAD_FOLDER']}/{username}/') or os.makedirs(
+                f'{current_app.config['UPLOAD_FOLDER']}/{username}/')
+
+            file.save(os.path.join(f'{current_app.config['UPLOAD_FOLDER']}/{username}/', filename))
 
             return (f'File {filename} uploaded successfully! '
                     f'<br> Original size: {original_size} bytes <br> '
-                    f'<a href="/ecg/download/{filename}">Download</a>')
+                    f'<a href="/ecg/download/{filename}">Download {filename}</a>')
 
     if 'username' in session:
         print(f'Logged in as {session["username"]}')
